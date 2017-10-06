c18a3c77bd29a2e5ba8fea9edb88a8b4712070ef
SOBREMESA\jcruiz <xekaruiz@gmail.com>
2017-10-06 20:00:55 +0200

    Subida del proyecto

---
+++ FifaLanzador/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace FifaLanzador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Asegurate de tener instalado Python en versión >= 3.7 y los componentes wmi y pypiwin32...");
+            Console.WriteLine("En caso de fallo, lanzar directamente desde CMD el fichero que se autogenera llamado main.py a través del comando py main.py y analizar la salida.");
+
+            if (!File.Exists("fifa15.exe"))
+            {
+                Console.WriteLine("No se encuentra el fichero fifa15.exe. Situar este ejecutable en la ruta en la que se encuentre el fifa15.exe.");
+                Console.WriteLine("Pulsar tecla para cerrar.");
+                Console.ReadKey();
+
+                return;
+            }
+
+            if (!File.Exists("main.py"))
+            {
+                CreatePythonLauncherScriptFromResource();
+            }
+
+            RunCMDCommand("py main.py");
+        }
+
+        private static void CreatePythonLauncherScriptFromResource()
+        {
+            string resourceName = "FifaLanzador.main.py";
+
+            if (!File.Exists("main.py"))
+            {
+
+                var assembly = Assembly.GetExecutingAssembly();
+                using (Stream input = assembly.GetManifestResourceStream(resourceName))
+                using (Stream output = File.Create("main.py"))
+                {
+                    byte[] buffer = new byte[8192];
+
+                    int bytesRead;
+                    while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        output.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+
+        public static String RunCMDCommand(string comando)
+        {
+            //Indicamos que deseamos inicializar el proceso cmd.exe junto a un comando de arranque. 
+            //(/C, le indicamos al proceso cmd que deseamos que cuando termine la tarea asignada se cierre el proceso).
+            //Para mas informacion consulte la ayuda de la consola con cmd.exe /? 
+            System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + comando);
+
+
+            // Indicamos que la salida del proceso se redireccione en un Stream
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            //Indica que el proceso no despliegue una pantalla negra (El proceso se ejecuta en background)
+            procStartInfo.CreateNoWindow = true;
+            //Esconder la ventana
+            //procStartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //Inicializa el proceso
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            //Consigue la salida de la Consola(Stream) y devuelve una cadena de texto
+            string result = proc.StandardOutput.ReadToEnd();
+
+            //Muestra en pantalla la salida del Comando
+            return result;
+        }
+    }
+}
---
+++ FifaLanzador/main.py
@@ -0,0 +1,83 @@
+"""Runs FIFA's config/launcher, presses enter, then closes the launcher. Run from same dir as FIFA's executable"""
+
+import os
+import re
+import wmi
+import win32gui
+import win32process
+import win32com.client
+
+
+def main():
+    # Init
+    c = wmi.WMI()
+    shell = win32com.client.Dispatch("WScript.Shell")
+    config_exe = "fifaconfig.exe"
+
+    # Detecting fifa executable
+    print ("Looking for FIFA executable...")
+    fifa_exe = ""
+    found_fifa = False
+    for file in os.listdir('.'):
+        if re.match('fifa[0-9]+(demo)?.exe', file, re.IGNORECASE):
+            found_fifa = True
+            fifa_exe = file
+
+    if not found_fifa:
+        print ("Unable to find FIFA executable, please run from the same directory as fifa*.exe")
+        os.system("pause")
+        return 1
+
+    # Run FIFA config/launcher
+    print("Running FIFA config/launcher...")
+    shell.run(os.path.join(os.path.dirname(__file__), fifa_exe))
+
+    # Get the HWND and PID of FIFA config/launcher
+    config_hwnds = []
+    config_process = None
+    print("Waiting for config/launcher to load...")
+    while len(config_hwnds) != 1:
+        for process in c.Win32_Process(name=config_exe):
+            config_hwnds = get_hwnds_for_pid(process.ProcessID)
+            config_process = process
+
+    # Give FIFA config/launcher focus
+    win32gui.SetForegroundWindow(config_hwnds[0])
+
+    print("Running FIFA...")
+    # Send an 'Enter' keypress
+    shell.SendKeys('{ENTER}', 0)
+
+    print("Waiting for FIFA to quit...")
+    # When FIFA quits, kill the config/launcher
+    while True:
+        for process in c.Win32_Process(name=fifa_exe):
+            watcher = c.watch_for(
+                notification_type="Deletion",
+                wmi_class="Win32_Process",
+                delay_secs=1,
+                ProcessId=process.ProcessID
+            )
+            watcher()
+            print ("Killing config/launcher...")
+            config_process.Terminate()
+
+            print ("All done. Cheers Jeff!")
+            return 0
+
+
+# Credit: Tim Golden - http://timgolden.me.uk/python/win32_how_do_i/find-the-window-for-my-subprocess.html
+def get_hwnds_for_pid(pid):
+    def callback(hwnd, hwnds):
+        if win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
+            _, found_pid = win32process.GetWindowThreadProcessId(hwnd)
+            if found_pid == pid:
+                hwnds.append(hwnd)
+        return True
+
+    hwnds = []
+    win32gui.EnumWindows(callback, hwnds)
+    return hwnds
+
+if __name__ == "__main__":
+    main()
